07c0213cc6909f145f7b7b6e51f41411bdbb8767
박준희 <junibibi04@gmail.com>
2019-09-18 01:07:35 +0900

    숫자판과 디스플레이 만들고, 디스플레이에 숫자 나타나도록 함

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func touchDigit(_ sender: UIButton) {
+        display.text = sender.currentTitle
+    }
 }
 
