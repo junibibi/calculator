68f422e41919ef3196541eda9ec605663078d3c7
박준희 <junibibi04@gmail.com>
2019-09-18 02:42:34 +0900

    연산부호가 눌렸을때 실행하는 코드를 작성해줌

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var userTouchingDigit = false
     
+    let calculator = Calculator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -34,6 +36,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchOperationSign(_ sender: UIButton) {
+        if userTouchingDigit {
+            if let number = Double(display.text!) {
+                calculator.setOperand(number: number)
+            }
+            userTouchingDigit = false
+        }
+        if let sign = sender.currentTitle {
+            calculator.performOperation(sign: sign)
+        }
+        display.text = String(calculator.result)
     }
 }
 
